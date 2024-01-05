66dbfdd851b8c1d043ae5b83a51c78c980e517f7
Молодец 2705 <ant_papa@mail.ru>
2024-01-05 13:54:31 +0600

    about grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,51 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Tecnologies from "./Tecnologies";
 
 function App() {
   return (
-    <div>
-        <Header />
-        <Tecnologies />
+    <div className='app-wrapper'>
+        <header className='header'>
+            <img src='https://avatars.mds.yandex.net/i?id=ceb925bc407a47345be2be6caf53b99e1c9bb330-9709143-images-thumbs&n=13'/>
+        </header>
+        <nav className='nav'>
+            <div>
+                <a>Profile</a>
+            </div>
+            <div>
+                <a>Messages</a>
+            </div>
+            <div>
+                <a>News</a>
+            </div>
+            <div>
+                <a>Music</a>
+            </div>
+            <div>
+                <a>Settings</a>
+            </div>
+        </nav>
+        <div className='content'>
+            <div>
+                <img src='https://q-cf.bstatic.com/images/hotel/max1024x768/776/77667840.jpg'/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My Posts
+                <div>
+                    New Post
+                </div>
+                <div>
+                    <div>
+                        Post 1
+                    </div>
+                    <div>
+                        Post 1
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
