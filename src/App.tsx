3d9553bc6f1cc5bdd5b038b6058de213d9be1fc7
Молодец 2705 <ant_papa@mail.ru>
2024-01-06 21:02:53 +0600

    about routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,31 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Music from "./components/Music/Music";
+import News from "./components/News/News";
+import Settings from "./components/Settings/Settings";
 
-function App() {
-  return (
-    <div className='app-wrapper'>
-        <Header />
-        <Navbar />
-        <div className='app-wrapper-content'>
-            <Dialogs />
-            {/*<Profile />*/}
-        </div>
-    </div>
-  );
+const App = () => {
+    return (
+        <BrowserRouter>
+
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Routes>
+                        <Route path='/dialogs' Component={Dialogs}/>
+                        <Route path='/profile' Component={Profile}/>
+                        <Route path='/music' Component={Music}/>
+                        <Route path='/news' Component={News}/>
+                        <Route path='/settings' Component={Settings}/>
+                    </Routes>
+                </div>
+            </div>
+
+        </BrowserRouter>);
 }
 
 export default App;
+
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,19 +4,19 @@ import s from "./Navbar.module.css";
 const Navbar = () => {
     return <nav className={s.nav}>
         <div className={s.item}>
-            <a>Profile</a>
+            <a href='/profile'>Profile</a>
         </div>
         <div className={`${s.item} ${s.active}`}>
-            <a>Messages</a>
+            <a href='/dialogs'>Messages</a>
         </div>
         <div>
-            <a>News</a>
+            <a href='/news'>News</a>
         </div>
         <div>
-            <a>Music</a>
+            <a href='/music'>Music</a>
         </div>
         <div>
-            <a>Settings</a>
+            <a href='/settings'>Settings</a>
         </div>
     </nav>
 }
